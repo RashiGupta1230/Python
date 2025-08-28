c2ea8ff4149e3bdd26786808d1d7df6d46c73d7a
RashiGupta1230 <cuterashigupta30@gmail.com>
2025-08-29 01:31:36 +0530

    Delete # 3DivisibleBy3LastDigitIs4.py

--- # 3DivisibleBy3LastDigitIs4.py
+++
@@ -1,7 +0,0 @@
-# 3. WAP to check if the number is divisible by 3 and the last digit is 4.
-n=int(input("Enter number: "))
-last_digit=n%10
-if n%3==0 and last_digit==4:
-    print("Yes")
-else:
-    print("No")
