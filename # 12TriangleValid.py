c2f22e9ac23ba4583b0365e475f409b4360b7aea
RashiGupta1230 <cuterashigupta30@gmail.com>
2025-08-29 01:30:28 +0530

    Delete # 12TriangleValid.py

--- # 12TriangleValid.py
+++
@@ -1,8 +0,0 @@
-# 12. You are given 3 integer angles of a triangle. Check if the triangle is valid or not.
-a=int(input("Enter first angle: "))
-b=int(input("Enter second angle: "))
-c=int(input("Enter third angle: "))
-if a+b+c==180:
-    print("Valid triangle")
-else:
-    print("Invalid triangle")
