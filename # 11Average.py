c86514d86987e91e3d70b821a5b055ca12366cb0
RashiGupta1230 <cuterashigupta30@gmail.com>
2025-08-29 01:44:35 +0530

    Delete # 11Average.py

--- # 11Average.py
+++
@@ -1,8 +0,0 @@
-# 11. Given 5 numbers A, B, C, D, E as input. Print their average.
-a=int(input("Enter number 1: "))
-b=int(input("Enter number 2: "))
-c=int(input("Enter number 3: "))
-d=int(input("Enter number 4: "))
-e=int(input("Enter number 5: "))
-avg=(a+b+c+d+e)/5
-print(avg)
